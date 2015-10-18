e61a39d5f3d0dd656609b7ae0a5151ac6c78d986
Yuri Mikhaylov <me@yurijmi.ru>
2015-10-19 00:27:19 +0300

    Created Add Friend View

---
+++ Friendbook Parse/AddFriendViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AddFriendViewController.swift
+//  Friendbook Parse
+//
+//  Created by Юрий Михайлов on 19.10.15.
+//  Copyright © 2015 Юрий Михайлов. All rights reserved.
+//
+
+import UIKit
+
+class AddFriendViewController: UIViewController {
+    
+    @IBOutlet weak var nameField     : UITextField!
+    @IBOutlet weak var ageField      : UITextField!
+    @IBOutlet weak var birthdayField : UITextField!
+    @IBOutlet weak var phoneField    : UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func cancelTapped(button: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func saveTapped(button: UIBarButtonItem) {
+        
+    }
+    
+}
